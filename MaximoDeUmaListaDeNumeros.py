038cfd43a858bc3e7aefbb79356811e5ac973378
Aluno <cassio.thiago1@gmail.com>
2019-02-21 08:44:49 -0300

    mensagem

---
+++ MaximoDeUmaListaDeNumeros.py
@@ -0,0 +1,13 @@
+def main():
+    ''' teste da funcao maximo '''
+
+def max_lista(lista):
+    ''' (list) -> int, int
+            recebe uma lista de inteiros e retorna o
+            valor maximo da lista e o indice onde ele ocorre '''
+
+    return 0, 0
+
+main()
+
+
